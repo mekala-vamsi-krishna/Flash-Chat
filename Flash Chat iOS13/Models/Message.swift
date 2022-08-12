d856a5b8c2f42d371cf8d09cad27a3421aff6740
 Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2022-08-12 10:00:12 +0530

    Complete FlashChat

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Flash Chat iOS13
+//
+//  Created by Mekala Vamsi Krishna on 23/07/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
